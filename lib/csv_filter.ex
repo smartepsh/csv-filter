8867f3a21928f70150ef760d1652b1eb2eecb994
Kenton Wang <smartepsh@gmail.com>
2018-08-12 13:31:06 +0800

    init project

---
+++ lib/csv_filter.ex
@@ -0,0 +1,18 @@
+defmodule CSVFilter do
+  @moduledoc """
+  Documentation for CSVFilter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CSVFilter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/csv_filter_test.exs
@@ -0,0 +1,8 @@
+defmodule CSVFilterTest do
+  use ExUnit.Case
+  doctest CSVFilter
+
+  test "greets the world" do
+    assert CSVFilter.hello() == :world
+  end
+end
